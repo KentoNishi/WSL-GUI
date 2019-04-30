bb9616a6277b5a521a3305da260d562419394ef1
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-04-29 19:18:21 -0700

    Removed cache

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 @echo on
-wsl eval 'TMP=$(mktemp -d);curl "https://github.com/KentoNishi/WSL-GUI/archive/v3.zip" -o "${TMP}/executable.zip" -J -L;unzip -o "${TMP}/executable.zip" -d "${TMP}/files";mv "${TMP}/files/WSL-GUI-3" "${TMP}/files/WSL-GUI"; cp -r "${TMP}/files/WSL-GUI/" "/mnt/c/users/%username%/"'
+wsl eval 'TMP=$(mktemp -d);curl "https://github.com/KentoNishi/WSL-GUI/archive/v3.zip" -H 'Cache-Control: no-cache' -o "${TMP}/executable.zip" -J -L;unzip -o "${TMP}/executable.zip" -d "${TMP}/files";mv "${TMP}/files/WSL-GUI-3" "${TMP}/files/WSL-GUI"; cp -r "${TMP}/files/WSL-GUI/" "/mnt/c/users/%username%/"'
 "%userprofile%\WSL-GUI\setup.bat"
